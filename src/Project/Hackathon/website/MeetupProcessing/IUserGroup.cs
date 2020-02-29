ad6e41f56d8dcab3ccd48be44c88a98f658ade48
Nikolay Mitikov <mitikov@users.noreply.github.com>
2020-02-29 15:50:59 +0200

    Shape user group abstraction

---
+++ src/Project/Hackathon/website/MeetupProcessing/IUserGroup.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ScDom.Project.Hackathon.MeetupProcessing
+{
+    /// <summary>
+    /// The user group details.
+    /// </summary>
+    public interface IUserGroup
+    {
+        /// <summary>
+        /// The name of the user group.
+        /// </summary>
+        string Name { get; }
+
+        /// <summary>
+        /// The identifier of the associated List Manager list.
+        /// </summary>
+        Guid AssociatedList { get; }
+    }
+}
