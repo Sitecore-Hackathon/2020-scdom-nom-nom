5d1a482bdfa22938880b3269d1e34d02b6375d85
Nikolay Mitikov <mitikov@users.noreply.github.com>
2020-02-29 13:48:00 +0100

    Task/11 automatically create list whenever user group is created (#23)
    * Change field type as list id would be stored as plain text
    
    * Define user group item
    
    * Welcome list manager package
    
    * Add handler that will create lists whenever user group item is created

---
+++ src/Project/Hackathon/website/UserGroupItem.cs
@@ -0,0 +1,15 @@
+﻿using Sitecore.Data.Fields;
+using Sitecore.Data.Items;
+using static ScDom.Project.Hackathon.Templates.UserGroup.Fields;
+
+namespace ScDom.Project.Hackathon
+{
+    public sealed class UserGroupItem : CustomItem
+    {
+        public UserGroupItem(Item innerItem) : base(innerItem)
+        {
+        }
+
+        public Field AssociatedList => InnerItem.Fields[ListReference];
+    }
+}
