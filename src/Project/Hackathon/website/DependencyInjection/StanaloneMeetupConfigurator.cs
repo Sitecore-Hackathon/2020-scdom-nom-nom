d4e09480599161176fd7d941c7a82a30e1930c58
Nikolay Mitikov <mitikov@users.noreply.github.com>
2020-02-29 15:52:21 +0200

    Add DI registration

---
+++ src/Project/Hackathon/website/DependencyInjection/StanaloneMeetupConfigurator.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using ScDom.Project.Hackathon.MeetupProcessing;
+using Sitecore.DependencyInjection;
+
+namespace ScDom.Project.Hackathon.DependencyInjection
+{
+    public sealed class StanaloneMeetupConfigurator : IServicesConfigurator
+    {
+        public void Configure(IServiceCollection serviceCollection)
+        {
+            serviceCollection.AddSingleton<MeetupManager, DefaultMeetupManager>();
+        }
+    }
+}
